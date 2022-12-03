2101a741a1ff7db3dab1ee6c961dff27421adef6
Sasha Berkoff <25355598+aberkoff@users.noreply.github.com>
2022-12-02 19:00:11 -0500

    updated my card catalog scraping and sql database interaction files

--- card_catalog_scraping.py
+++ card_catalog_scraping.py
@@ -1,13 +1,13 @@
 import pandas as pd
-#from selenium import webdriver
 import json
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import geckodriver_autoinstaller
 import random
 from retrying import retry
-#from selenium.webdriver.common.by import By
+import argparse
+import logging
+import postgres_interaction
+
 
 
 def book_tuple(k, v):
@@ -21,20 +21,11 @@ def book_dict(k, v):
     d['title'] = briefInfo['title']
     authors_list = briefInfo['authors']
     d['authors'] = authors_list
-    #print(d)
-    # d['author_one'] = authors_list[0]
-    # if len(authors_list) >= 2:
-    #     d['author_two'] = authors_list[1]
-    # if len(authors_list) >=3:
-    #     d['author_three'] = authors_list[2]
     return d
 
 @retry(wait_random_min=500, wait_random_max=2000, stop_max_attempt_number=3)
-def scrape_page(page_num):
+def scrape_results_page(page_num):
     try:
-        #geckodriver_autoinstaller.install()
-        #driver = webdriver.Firefox()
-        #"https://seattle.bibliocommons.com/v2/search?custom_edit=false&query=formatcode%3A(AB%20)&searchType=bl&suppress=true&f_FORMAT=AB&sort=author&page=1"
         resp=requests.get('https://seattle.bibliocommons.com/v2/search', 
                       params = {'custom_edit': 'false',
                                 'query':'formatcode:(AB )',
@@ -43,14 +34,11 @@ def scrape_page(page_num):
                                 'sort':'author',
                                 'page':str(page_num)
                                })
-        # driver.get("https://seattle.bibliocommons.com/v2/search?custom_edit=false&query=formatcode%3A(AB%20)&searchType=bl&suppress=true&f_FORMAT=AB&sort=author&page={}".format(page_num))
+
         soup = BeautifulSoup(resp.content, "html.parser")
-        #soup = BeautifulSoup(driver.page_source, "html.parser")
-        
         book_info_json = soup.find('script', attrs={'type':'application/json',
                                            'data-iso-key':'_0'
                                       })
-
         book_info = json.loads(book_info_json.text)
         book_list = book_info['entities']['bibs']
         book_dicts = list() 
@@ -58,30 +46,47 @@ def scrape_page(page_num):
         for k, v in book_list.items():
             book_dicts.append(book_dict(k,v))
 
-
         df = pd.DataFrame(book_dicts)
         df.set_index('id_num', inplace = True)
         
         return df
     except requests.exceptions.RequestException as e:  # This is the correct syntax
-        print("page: {}".format(page_num))
-        print(e)
+        logging.warning("Unexpected {}, {} on page {}".format(e, type(e), page_num))
 
 
+def scrape_card_catalog(engine, start, end):
+    # last page is 5607
+	if end < 0:
+		end = 5608
+	for page_num in range(start, end):
+		if page_num%100 == 0:
+			logging.info("page {}".format(page_num))
+		df = scrape_results_page(page_num)
+		postgres_interaction.add_record(engine, df, 'book_ids')
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("start", type=int,
+								help="which result page to start start scraping")
+	parser.add_argument("end", type=int,
+								help="which results page to stop scraping")
+	args = parser.parse_args()
+	logfile = 'logs/card_catalog_scraping.log'
+	logging.basicConfig(filename=logfile, 
+								level=logging.INFO,
+								format = '%(asctime)s %(message)s'
+								)
+								
+	logging.info("scraping catalog pages # {} through {}...".format(args.start, args.end))
+	engine = postgres_interaction.get_db_engine()
+	scrape_card_catalog(engine, args.start, args.end)
+	logging.info("done")
+	logging.info("-------------------")
 
 
 if __name__ == "__main__":
-    for page in range(1,2):
-        rows = scrape_page(page)
-        with engine.connect() as conn:
-            result = conn.execute(
-                db.insert(book_ids), rows,
-            )
-            conn.commit()
-    
-
-    
-    
+    main()
           
 
 
--- postgres_interaction.py
+++ postgres_interaction.py
@@ -1,61 +1,113 @@
 import sqlalchemy as db
-from card_catalog_scraping import scrape_page
-from sqlalchemy import text
+from sqlalchemy import text, MetaData
 import pandas as pd
 import time
-#from selenium import webdriver
-#import geckodriver_autoinstaller
-
-# engine = db.create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-# conn = psycopg2.connect(
-#     "postgresql://gbgqpmhx:PUajiD2a9AFI8bDDgMGdn_Sb_SkArTz0@peanut.db.elephantsql.com/gbgqpmhx"
-# )
+import os
+import logging
+import argparse
 
 
-def make_id_table(engine):
+def drop_table(engine, table_name):
     with engine.connect() as conn:
         conn.execute(text(
             """
-            DROP TABLE IF EXISTS book_ids
-            """
+            DROP TABLE IF EXISTS {}
+            """.format(table_name)
         ))
-        # conn.execute(text(
-        #     """
-        #     CREATE TABLE book_ids (id_num text PRIMARY KEY, 
-        #                title text, 
-        #                authors text[]);
-        #     """
-        # ))
-
+        
+        
+def add_record(engine, df, table_name):
+    with engine.connect() as conn:
+        df.to_sql(table_name, conn, if_exists = 'append')
         conn.commit()
         
+def read_head(engine, table, head_len = 10):
+	with engine.connect() as conn:
+		select_statement = table.select().fetch(head_len)
+		result_set = conn.execute(select_statement)
+		for r in result_set:
+			print(r)
+		conn.commit()
+
+def get_book_ids(engine):
+	with engine.connect() as conn:
+		logging.info('reading book_ids into a dataframe')
+		book_ids = pd.read_sql_table('book_ids', conn)
+		# book_ids.to_pickle("./book_ids.pkl")
+		conn.commit()
+	return book_ids
+
+	
+
+def get_db_engine():
+	connection_url = os.environ['POSTGRES_CONN']
+	try:
+		logging.info('connecting to the database.')
+		engine = db.create_engine(connection_url, echo=False, future=True)
+		return engine
+	except Exception as e :
+		logging.warning("Exception: {} of type: {} when connecting to the database".format(e, type(e)))
+
+
+
+table_names = ['book_ids', 'book_features', 'related_books']
+
+
+def get_valid_table_name(name):
+	while name not in(table_names):
+		print("that is not a table in our database. our tables are:")
+		print("\n".join(table_names))
+		name = input("please select a table: ")
+	print("thank you. you have selected: {}".format(name))
+	return name
+
 def main():
-    engine = db.create_engine("postgresql://gbgqpmhx:PUajiD2a9AFI8bDDgMGdn_Sb_SkArTz0@peanut.db.elephantsql.com/gbgqpmhx", echo=False, future=True)
-    make_id_table(engine)
-    # metadata_obj = db.MetaData()
-    
-    #geckodriver_autoinstaller.install()
-    #driver = webdriver.Firefox()
-    # last page is 5607
-    for page_num in range(1, 5608):
-        if page_num%100 == 0:
-            print("page {}".format(page_num))
-        #time.sleep(0.5)
-        #df = scrape_page(page_num, driver)
-        df = scrape_page(page_num)
-        with engine.connect() as conn:
-            df.to_sql('book_ids', conn, if_exists = 'append')
-            conn.commit()
-    driver.quit()
-    # with engine.connect() as conn:
-    #     df = pd.read_sql_table('book_ids', conn)
-        
-    # print(df)
-        
-    
+	parser = argparse.ArgumentParser()
+	group = parser.add_mutually_exclusive_group()	
+	group.add_argument("-r", "--read", help="read sql table", action="store_true")
+	group.add_argument("-w", "--write", help="write sql table", action="store_true")
+	group.add_argument("-d", "--delete", help="delete sql table", action="store_true")
+	parser.add_argument("table_name", help="the sql table name")
+
+	args = parser.parse_args()
+	logfile = 'logs/postgres_interaction.log'
+	
+	logging.basicConfig(filename=logfile, 
+								level=logging.INFO,
+								format = '%(asctime)s %(message)s'
+								)
+	table_name = get_valid_table_name(args.table_name)
+	engine = get_db_engine()
+	metadata_obj = MetaData()
+	metadata_obj.reflect(bind=engine)
+	
+	if args.write:
+		logging.info('writing to table {}'.format(table_name))
+		filename = input("enter path for the pickled dataframe to write to {}:\n".format(table_name))
+		df = pd.read_pickle(filename)
+		add_record(engine, df, table_name)
+	elif args.delete:
+		logging.info('confirming deletion of table {}'.format(table_name))
+		yes_no = input('are you sure you want to delete {}? y/n: '.format(table_name))
+		if lower(yes_no) in {'y', 'yes'}:
+			drop_table(engine, book_ids)
+	elif args.read:
+		logging.info('reading from table {}'.format(table_name))
+		table = metadata_obj.tables[table_name]
+		read_head(engine, table)
+	else:
+		logging.info('no option selected')
+		print('no valid option selected. please run again.')
+				
+
+	logging.info("done")
+	logging.info("-------------------")
+
 
 if __name__ == "__main__":
     main()
+	
     
+
 
 
